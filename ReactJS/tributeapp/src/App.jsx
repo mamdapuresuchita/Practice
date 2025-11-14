045d74c317f00d121e81d9f3c551c839dc4b946b
Suchita <mamdapuresuchita@gmail.com>
2025-11-14 09:46:19 +0530

    Added go to top

--- ReactJS/tributeapp/src/App.jsx
+++ ReactJS/tributeapp/src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './Components/Footer'
 import Achievements from './Components/Achievements'
 import Comments from './Components/Comments'
 import { useEffect, useState } from 'react'
-import { FaArrowUp } from "react-icons/fa"
+import { TiArrowUpOutline } from "react-icons/ti";
 
 function App() {
   const [showGoTop, setShowGoTop] = useState(false);
@@ -34,16 +34,17 @@ function App() {
       <Quotes />
       <Achievements />
       <Comments />
-      <Footer />
+      
 
       {showGoTop && (
         <button
           onClick={scrollToTop}
-          className="fixed bottom-6 right-6 bg-indigo-600 text-white p-3 rounded-full shadow-lg hover:bg-indigo-700 transition-all"
-        >
-          <FaArrowUp size={20} />
+          className='goToTop'
+          >
+          <TiArrowUpOutline  size={25} />
         </button>
       )}
+      <Footer />
     </ThemeProvider>
   )
 }
